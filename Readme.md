f0f37844937411036d188decd403e81fc59a2bf4
dinnubunny <dineshkandula007@gmail.com>
2023-08-25 22:52:53 +0530

    First Commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+***My Personal Express JS Project***
---
+++ app.mjs
@@ -0,0 +1,22 @@
+import express from 'express'
+// import addDays from "date-fns/addDays";
+
+// const result = addDays(new Date(2000, 0, 20))
+// console.log(result);
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("Hello World");
+});
+
+app.get('/login', (req, res) => {
+  res.send("Login Page");
+});
+
+const port = 3005;
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}/`);
+});
+
+export default app;
